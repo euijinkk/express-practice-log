a807495d4cff391054ed666b5e50de1eefdf815a
euijinkk <gytks4@naver.com>
2023-09-26 22:29:54 +0900

    feat: cors

--- app.ts
+++ app.ts
@@ -7,14 +7,20 @@ import logger from "morgan";
 
 import usersRouter from "./routes/users";
 import swaggerRouter from "./swagger";
+import cors from "cors";
 
 const app = express();
+// 특정 도메인만 허용하려면 다음과 같이 설정합니다:
+// app.use(cors({
+//   origin: 'https://example.com'
+// }));
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/users", usersRouter);
 app.use(swaggerRouter);
@@ -24,11 +30,6 @@ connectDB();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
-
-app.get("/123", (req, res) => {
-  console.log(req);
-  res.send("Hello, World!");
 });
 
 export default app;
