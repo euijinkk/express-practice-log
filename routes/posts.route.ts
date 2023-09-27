abc5a5a97194bd5aac9a4fa9a5352370a304a098
gytks4 <gytks4@naver.com>
2023-09-28 00:19:51 +0900

    feat: express-validator를 posts routes에 적용

--- routes/posts.route.ts
+++ routes/posts.route.ts
@@ -1,8 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import Post from "../models/posts.model";
 import User from "../models/users.model";
+import { body, param, validationResult } from "express-validator";
 
 const router = express.Router();
+
+// 게시물 생성 시의 유효성 검사 미들웨어
+const createPostValidation = [
+  body("title").trim().notEmpty().withMessage("제목을 입력하세요."),
+  body("content").trim().notEmpty().withMessage("내용을 입력하세요."),
+  body("userId").isMongoId().withMessage("올바른 사용자 ID가 필요합니다."),
+];
+
+// 게시물 수정 시의 유효성 검사 미들웨어
+const updatePostValidation = [
+  param("id").isMongoId().withMessage("올바른 게시물 ID가 필요합니다."),
+  body("title").trim().notEmpty().withMessage("제목을 입력하세요."),
+  body("content").trim().notEmpty().withMessage("내용을 입력하세요."),
+  body("userId").isMongoId().withMessage("올바른 사용자 ID가 필요합니다."),
+];
 
 router.get("/", async (req, res) => {
   const authorId = req.query.authorId;
@@ -34,7 +50,13 @@ router.get("/:id", async (req, res) => {
   res.status(200).send(post);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", createPostValidation, async (req: Request, res: Response) => {
+  // express-validator에서 유효성 검사 수행
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const body = req.body;
   const userId = body.userId;
 
@@ -65,26 +87,31 @@ router.delete("/:id", async (req, res) => {
   res.status(200).send({ message: "success" });
 });
 
-router.put("/:id", async (req, res) => {
-  const body = req.body;
-  const userId = body.userId;
-  const id = req.params.id;
+router.put(
+  "/:id",
+  updatePostValidation,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  const post = await Post.findById(id);
-  if (post && post.author._id.toString() !== userId) {
-    return res.status(401).send({ message: "수정 권한이 없습니다." });
+    const body = req.body;
+    const userId = body.userId;
+    const id = req.params.id;
+
+    const post = await Post.findById(id);
+    if (post && post.author._id.toString() !== userId) {
+      return res.status(401).send({ message: "수정 권한이 없습니다." });
+    }
+
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: id, author: userId },
+      body
+    );
+
+    res.status(200).send(updatedPost);
   }
-
-  if (body.title == null || body.content == null) {
-    return res.status(400).send({ message: "title, content를 채워주세요" });
-  }
-
-  const updatedPost = await Post.findOneAndUpdate(
-    { _id: id, author: userId },
-    body
-  );
-
-  res.status(200).send(updatedPost);
-});
+);
 
 export default router;
